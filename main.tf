2086a5a962db01d235e6a6d556d69dc9b7f974cf
Máté Lang <mate@MacBook-Pro.localdomain>
2021-04-29 19:01:10 +0300

    First commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.37.0"
+    }
+  }
+}
+
+data "terraform_remote_state" "dns" {
+  backend = "remote"
+
+  config = {
+    organization = "matelang"
+    workspaces = {
+      name = "hwsw-tf-cloud-dns"
+    }
+  }
+}
+
+resource "aws_route53_record" "record" {
+  name = "record"
+  type = "CNAME"
+  records = ["binhatch.com"]
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
+}
